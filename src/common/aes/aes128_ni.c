c5dff41709dda76044433b49934bb9e24da7b650
Douglas Stebila <dstebila@uwaterloo.ca>
2020-07-16 17:03:50 -0400

    Try interleaving AES 8x

--- src/common/aes/aes128_ni.c
+++ src/common/aes/aes128_ni.c
@@ -89,6 +89,78 @@ void oqs_aes128_enc_sch_block_ni(const uint8_t *plaintext, const void *_schedule
 	aes128ni_encrypt(schedule, plaintext, ciphertext);
 }
 
+void oqs_aes128_enc_sch_block8x_ni(const uint8_t *plaintext, const void *_schedule, uint8_t *ciphertext) {
+
+	const __m128i *rkeys = (const __m128i *) _schedule;
+
+	const uint8_t *pt0 = plaintext + 0 * 16;
+	const uint8_t *pt1 = plaintext + 1 * 16;
+	const uint8_t *pt2 = plaintext + 2 * 16;
+	const uint8_t *pt3 = plaintext + 3 * 16;
+	const uint8_t *pt4 = plaintext + 4 * 16;
+	const uint8_t *pt5 = plaintext + 5 * 16;
+	const uint8_t *pt6 = plaintext + 6 * 16;
+	const uint8_t *pt7 = plaintext + 7 * 16;
+
+	uint8_t *ct0 = ciphertext + 0 * 16;
+	uint8_t *ct1 = ciphertext + 1 * 16;
+	uint8_t *ct2 = ciphertext + 2 * 16;
+	uint8_t *ct3 = ciphertext + 3 * 16;
+	uint8_t *ct4 = ciphertext + 4 * 16;
+	uint8_t *ct5 = ciphertext + 5 * 16;
+	uint8_t *ct6 = ciphertext + 6 * 16;
+	uint8_t *ct7 = ciphertext + 7 * 16;
+
+	__m128i nv0 = _mm_load_si128((const __m128i *) pt0);
+	__m128i nv1 = _mm_load_si128((const __m128i *) pt1);
+	__m128i nv2 = _mm_load_si128((const __m128i *) pt2);
+	__m128i nv3 = _mm_load_si128((const __m128i *) pt3);
+	__m128i nv4 = _mm_load_si128((const __m128i *) pt4);
+	__m128i nv5 = _mm_load_si128((const __m128i *) pt5);
+	__m128i nv6 = _mm_load_si128((const __m128i *) pt6);
+	__m128i nv7 = _mm_load_si128((const __m128i *) pt7);
+
+	__m128i temp0 = _mm_xor_si128(nv0, rkeys[0]);
+	__m128i temp1 = _mm_xor_si128(nv1, rkeys[0]);
+	__m128i temp2 = _mm_xor_si128(nv2, rkeys[0]);
+	__m128i temp3 = _mm_xor_si128(nv3, rkeys[0]);
+	__m128i temp4 = _mm_xor_si128(nv4, rkeys[0]);
+	__m128i temp5 = _mm_xor_si128(nv5, rkeys[0]);
+	__m128i temp6 = _mm_xor_si128(nv6, rkeys[0]);
+	__m128i temp7 = _mm_xor_si128(nv7, rkeys[0]);
+
+#pragma unroll(9)
+	for (int i = 1 ; i < 10 ; i++) {
+		temp0 = _mm_aesenc_si128(temp0, rkeys[i]);
+		temp1 = _mm_aesenc_si128(temp1, rkeys[i]);
+		temp2 = _mm_aesenc_si128(temp2, rkeys[i]);
+		temp3 = _mm_aesenc_si128(temp3, rkeys[i]);
+		temp4 = _mm_aesenc_si128(temp4, rkeys[i]);
+		temp5 = _mm_aesenc_si128(temp5, rkeys[i]);
+		temp6 = _mm_aesenc_si128(temp6, rkeys[i]);
+		temp7 = _mm_aesenc_si128(temp7, rkeys[i]);
+	}
+
+	temp0 = _mm_aesenclast_si128(temp0, rkeys[10]);
+	temp1 = _mm_aesenclast_si128(temp1, rkeys[10]);
+	temp2 = _mm_aesenclast_si128(temp2, rkeys[10]);
+	temp3 = _mm_aesenclast_si128(temp3, rkeys[10]);
+	temp4 = _mm_aesenclast_si128(temp4, rkeys[10]);
+	temp5 = _mm_aesenclast_si128(temp5, rkeys[10]);
+	temp6 = _mm_aesenclast_si128(temp6, rkeys[10]);
+	temp7 = _mm_aesenclast_si128(temp7, rkeys[10]);
+
+	_mm_store_si128((__m128i *) ct0, temp0);
+	_mm_store_si128((__m128i *) ct1, temp1);
+	_mm_store_si128((__m128i *) ct2, temp2);
+	_mm_store_si128((__m128i *) ct3, temp3);
+	_mm_store_si128((__m128i *) ct4, temp4);
+	_mm_store_si128((__m128i *) ct5, temp5);
+	_mm_store_si128((__m128i *) ct6, temp6);
+	_mm_store_si128((__m128i *) ct7, temp7);
+
+}
+
 // From crypto_core/aes128decrypt/dolbeau/aesenc-int
 static inline void aes128ni_decrypt(const __m128i rkeys[11], const unsigned char *n, unsigned char *out) {
 	__m128i nv = _mm_load_si128((const __m128i *)n);
