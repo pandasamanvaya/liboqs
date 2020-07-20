e92d6ef86615c0a9adcfe1d6ca4bf3532f398a3c
Douglas Stebila <dstebila@uwaterloo.ca>
2020-07-20 09:27:58 -0400

    Fix key schedule usage for NI

--- src/common/aes/aes_local.h
+++ src/common/aes/aes_local.h
@@ -2,7 +2,7 @@
 
 #include <stdint.h>
 
-void oqs_aes128_load_schedule_ni(const uint8_t *key, void **_schedule);
+void oqs_aes128_load_schedule_ni(const uint8_t *key, void **_schedule, int for_encryption);
 void oqs_aes128_free_schedule_ni(void *schedule);
 void oqs_aes128_enc_sch_block_ni(const uint8_t *plaintext, const void *_schedule, uint8_t *ciphertext);
 void oqs_aes128_enc_sch_block8x_ni(const uint8_t *plaintext, const void *_schedule, uint8_t *ciphertext);
@@ -10,7 +10,7 @@ void oqs_aes128_dec_sch_block_ni(const uint8_t *ciphertext, const void *_schedul
 void oqs_aes128_ecb_enc_sch_ni(const uint8_t *plaintext, const size_t plaintext_len, const void *schedule, uint8_t *ciphertext);
 void oqs_aes128_ecb_dec_sch_ni(const uint8_t *plaintext, const size_t plaintext_len, const void *schedule, uint8_t *ciphertext);
 
-void oqs_aes128_load_schedule_c(const uint8_t *key, void **_schedule, UNUSED int for_encryption);
+void oqs_aes128_load_schedule_c(const uint8_t *key, void **_schedule, int for_encryption);
 void oqs_aes128_free_schedule_c(void *schedule);
 void oqs_aes128_enc_sch_block_c(const uint8_t *plaintext, const void *_schedule, uint8_t *ciphertext);
 void oqs_aes128_dec_sch_block_c(const uint8_t *ciphertext, const void *_schedule, uint8_t *plaintext);
@@ -24,7 +24,7 @@ void oqs_aes256_dec_sch_block_ni(const uint8_t *ciphertext, const void *_schedul
 void oqs_aes256_ecb_enc_sch_ni(const uint8_t *plaintext, const size_t plaintext_len, const void *schedule, uint8_t *ciphertext);
 void oqs_aes256_ecb_dec_sch_ni(const uint8_t *plaintext, const size_t plaintext_len, const void *schedule, uint8_t *ciphertext);
 
-void oqs_aes256_load_schedule_c(const uint8_t *key, void **_schedule, UNUSED int for_encryption);
+void oqs_aes256_load_schedule_c(const uint8_t *key, void **_schedule, int for_encryption);
 void oqs_aes256_free_schedule_c(void *schedule);
 void oqs_aes256_enc_sch_block_c(const uint8_t *plaintext, const void *_schedule, uint8_t *ciphertext);
 void oqs_aes256_dec_sch_block_c(const uint8_t *ciphertext, const void *_schedule, uint8_t *plaintext);
